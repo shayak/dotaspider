f2555deb7f8049bc01517633a1215d28ba66da26
Shayak Dutta <shayak007@gmail.com>
2015-07-18 14:32:38 -0400

    refactoring

--- dotaspider/spiders/Dota2VPGame_spider.py
+++ dotaspider/spiders/Dota2VPGame_spider.py
@@ -1,26 +1,27 @@
 import scrapy
 import json
+from models import StartupData
+from models import Xpaths
 from dotaspider.utility import helper
 from dotaspider.data import data
 from dotaspider.items import DotaspiderItem, DotaSpiderResult
 
 class Dota2VPGameSpider(scrapy.Spider):
-    name="dota2vpgame"
-    baseurl = "http://dota2.vpgame.com"
-    allowed_domains = ["dota2.vpgame.com"]
-    start_urls = [baseurl + "/guess.html"]
+    name=StartupData.name
+    baseurl = StartupData.baseurl
+    allowed_domains = StartupData.allowed_domains
+    start_urls = StartupData.start_urls
     
-    bets_list_xpath = "//div[@class='items']/a"
-    handicap_xpath = "//ul[@class='spinach-tap clearfix']//li/a[text()='Handicap']"
+    bets_list_xpath = Xpaths.bets_list
 
     def parse(self, response):
-        pages_xpath = "//div[@class='pager']//li[starts-with(@class, 'page')]/a"
+        pages_xpath = Xpaths.pager
         resp = response.xpath(pages_xpath)
         if len(resp)==0:
             yield scrapy.Request(self.start_urls[0], callback=self.parsepage)
         else:
             for sel in response.xpath(pages_xpath):
-                path = sel.xpath('@href').extract()[0]
+                path = sel.extract()[0]
                 yield scrapy.Request(self.baseurl+path, callback=self.parsepage)
 
     def parsepage(self, response):
@@ -36,34 +37,37 @@ class Dota2VPGameSpider(scrapy.Spider):
                 yield scrapy.Request(self.baseurl+tup[1], callback=self.parsebetpage)
     
     def parsebetpage(self, response):
-        for sel in response.xpath(self.handicap_xpath):
-            handicap_link = self.baseurl+sel.xpath('@href').extract()[0]
+        handicap_link_xpath = Xpaths.handicap_link
+        for sel in response.xpath(handicap_link_xpath):
+            handicap_link = self.baseurl+sel.extract()
+            print " XPATH = " + handicap_link_xpath
+            print " URL = " + response.url
+            print 'crawling: ' + handicap_link
             yield scrapy.Request(handicap_link, callback=lambda r, l=handicap_link:self.parsebet(r, l))
 
     def parsebet(self, response, link):
-        schedule_xpath = "//div[@class='spinach-corps']//div[@class='spinach-item-tt']/p[@class='pull-right']"
-        data_xpath = "//div[@class='spinach-corps-data']"
-        handicap_xpath = "//div[@class='spinach-corps']//a[contains(@title,'handicap')]/span[@class='f-warning']"
-        bestof_xpath = "//div[@class='spinach-corps']//div[@class='spinach-corps-vs']//span[@class='f-14']"
+        schedule_xpath = Xpaths.schedule
+        handicap_xpath = Xpaths.handicap
+        bestof_xpath = Xpaths.bestof
 
         if len(response.xpath(schedule_xpath+"/span[text()='Cleared']")) > 0:
             return
         
-        odds_xpath = data_xpath + "//span[@class='vp-item-odds']/text()"
-        teams_xpath = data_xpath + "//p[@class='spinach-corps-name ellipsis']/@title"
+        odds_xpath = Xpaths.odds
+        teams_xpath = Xpaths.teams
 
         odds = map(lambda x: float(x.strip()), response.xpath(odds_xpath).extract())
         teams = response.xpath(teams_xpath).extract()
         
         h = response.xpath(handicap_xpath + '/text()').extract()[0]
         hteam = h[h.find("[")+1:h.find("-")].strip()
-        handicap = h[h.find("-")+1:h.find("]")].strip()
+        hamount = float(h[h.find("-")+1:h.find("]")].strip())
 
-        bestof = response.xpath(bestof_xpath+"/text()").extract()[0]
+        bestof = response.xpath(bestof_xpath).extract()[0]
         
         dic = DotaSpiderResult()
         dic['handicap_team'] = hteam
-        dic['handicap'] = float(handicap)
+        dic['handicap'] = hamount
         dic['team1'] = teams[0]
         dic['odds1'] = odds[0]
         dic['team2'] = teams[1]
--- dotaspider/spiders/models.py
+++ dotaspider/spiders/models.py
@@ -4,19 +4,19 @@ class StartupData:
     allowed_domains = ["dota2.vpgame.com"]
     start_urls = [baseurl + "/guess.html"]
 
-class SelectorXPaths:
+class Xpaths:
     #guess.html
-    pager = "//div[@class='pager']//li[@starts-with(@class, 'page')]/a/@href"
+    pager = "//div[@class='pager']//li[starts-with(@class, 'page')]/a/@href"
 
     #match listings page
     bets_list = "//div[@class='items']/a"
 
     #individual match base page
-    handicap = "//ul[@class='spinach-tap clearfix']//li/a[text()='Handicap']/@href"
+    handicap_link = "//ul[@class='spinach-tap clearfix']//li/a[text()='Handicap']/@href"
 
     #individual bet page
     schedule = "//div[@class='spinach-corps']//div[@class='spinach-item-tt']/p[@class='pull-right']"
     odds = "//div[@class='spinach-corps-data']//span[@class='vp-item-odds']/text()"
     teams = "//div[@class='spinach-corps-data']//p[@class='spinach-corps-name ellipsis']/@title"
-    handicap_amt = "//div[@class='spinach-corps']//a[contains(@title,'handicap')]/span[@class='f-warning']"
+    handicap = "//div[@class='spinach-corps']//a[contains(@title,'handicap')]/span[@class='f-warning']"
     bestof = "//div[@class='spinach-corps']//div[@class='spinach-corps-vs']//span[@class='f-14']/text()"
